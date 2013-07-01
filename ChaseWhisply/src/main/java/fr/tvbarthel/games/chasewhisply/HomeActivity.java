85667d6fb757af3efd985a12a18446be0754bd51
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-01 18:02:21 +0200

    Rename 2

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
@@ -9,7 +9,7 @@ public class HomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 
 
