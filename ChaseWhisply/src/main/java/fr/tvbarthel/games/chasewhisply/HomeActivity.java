2f5383f8fa7f70dd6b0e8e5b48ddadb2127ba1c6
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-01 18:01:07 +0200

    Rename

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class HomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
