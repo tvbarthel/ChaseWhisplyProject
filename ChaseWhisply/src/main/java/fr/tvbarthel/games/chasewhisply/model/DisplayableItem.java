c3c7ac266482355b2de22c4297bdbb8e9857db11
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-02 11:18:34 +0200

    Add a base class for displayable items

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/DisplayableItem.java
@@ -0,0 +1,107 @@
+package fr.tvbarthel.games.chasewhisply.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ *
+ */
+public class DisplayableItem implements Parcelable{
+	// x coordinate
+	private int mX;
+	// y coordinate
+	private int mY;
+	// width
+	private int mWidth;
+	// height
+	private int mHeight;
+	// tells if the item is active/visible
+	private boolean mIsActive;
+
+
+	protected DisplayableItem(Parcel in){
+		readFromParcel(in);
+	}
+
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int i) {
+		out.writeInt(mX);
+		out.writeInt(mY);
+		out.writeInt(mWidth);
+		out.writeInt(mHeight);
+		out.writeByte((byte) (mIsActive ? 1 : 0));
+	}
+
+	/**
+	 * read parameters from a given Parcel
+	 *
+	 * @param in input Parcel to read from
+	 */
+	public void readFromParcel(Parcel in){
+		this.mX = in.readInt();
+		this.mY = in.readInt();
+		this.mWidth = in.readInt();
+		this.mHeight = in.readInt();
+		this.mIsActive = in.readByte() == 1;
+	}
+
+	public static final Parcelable.Creator<DisplayableItem> CREATOR = new Parcelable.Creator<DisplayableItem>() {
+		public DisplayableItem createFromParcel(Parcel in) {
+			return new DisplayableItem(in);
+		}
+
+		public DisplayableItem[] newArray(int size) {
+			return new DisplayableItem[size];
+		}
+	};
+
+	/**
+	 * Getters and Setters
+	 */
+	public int getX() {
+		return mX;
+	}
+
+	public void setX(int mX) {
+		this.mX = mX;
+	}
+
+	public int getY() {
+		return mY;
+	}
+
+	public void setY(int mY) {
+		this.mY = mY;
+	}
+
+	public int getWidth() {
+		return mWidth;
+	}
+
+	public void setWidth(int mWidth) {
+		this.mWidth = mWidth;
+	}
+
+	public int getHeight() {
+		return mHeight;
+	}
+
+	public void setHeight(int mHeight) {
+		this.mHeight = mHeight;
+	}
+
+	public boolean isActive() {
+		return mIsActive;
+	}
+
+	public void setActive(boolean active) {
+		mIsActive = active;
+	}
+
+}
