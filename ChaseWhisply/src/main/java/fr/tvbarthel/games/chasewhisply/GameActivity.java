8308ae7f94e47d9fa0f6ffa6635a97043f179319
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-03 21:11:13 +0200

    Add GameActivity skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/GameActivity.java
@@ -0,0 +1,22 @@
+package fr.tvbarthel.games.chasewhisply;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+}
