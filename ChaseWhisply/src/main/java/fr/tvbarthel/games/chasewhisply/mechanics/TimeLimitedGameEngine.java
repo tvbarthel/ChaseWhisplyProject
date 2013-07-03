a642090e448654fffc528411da8585b41975bdca
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 18:35:49 +0200

    Update TimeLimitedGameEngine

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/TimeLimitedGameEngine.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/TimeLimitedGameEngine.java
@@ -1,8 +1,39 @@
 package fr.tvbarthel.games.chasewhisply.mechanics;
 
-public class TimeLimitedGameEngine extends GameEngine{
+public class TimeLimitedGameEngine extends GameEngine implements GameTimer.IGameTimer{
+	private GameTimer mGameTimer;
 
 	public TimeLimitedGameEngine(GameInformation gameInformation) {
 		super(gameInformation);
+		mGameTimer = new GameTimer(gameInformation.getRemainingTime(), this);
+	}
+
+	@Override
+	public void startGame() {
+		super.startGame();
+		mGameTimer.startTimer();
+	}
+
+	@Override
+	public void pauseGame() {
+		super.pauseGame();
+		mGameInformation.setRemainingTime(mGameTimer.stopTimer());
+	}
+
+	@Override
+	public void resumeGame() {
+		super.resumeGame();
+		mGameTimer.startTimer();
+	}
+
+	@Override
+	public void stopGame() {
+		super.stopGame();
+		mGameInformation.setRemainingTime(0);
+	}
+
+	@Override
+	public void timerEnd() {
+		stopGame();
 	}
 }
