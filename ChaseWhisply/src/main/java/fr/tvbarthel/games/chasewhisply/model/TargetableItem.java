ee6648482c8f221ecb709f57972cb2f381ccc694
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 09:03:13 +0200

    Add isTargetable & hit

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/TargetableItem.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/TargetableItem.java
@@ -2,13 +2,42 @@ package fr.tvbarthel.games.chasewhisply.model;
 
 import android.os.Parcel;
 
-public class TargetableItem extends DisplayableItem {
+abstract public class TargetableItem extends DisplayableItem {
+	//health of the item
+	protected int mHealth;
 
 	protected TargetableItem(Parcel in) {
 		super(in);
 	}
 
-	public boolean isTargetable(int crossHairX, int crossHairY) {
-		return (crossHairX > mX && crossHairX < (mX + mWidth) && crossHairY > mY && crossHairY < (mY + mHeight));
+	/**
+	 * check if the item is under the crosshairs
+	 *
+	 * @param crosshairsX
+	 * @param crosshairsY
+	 * @return
+	 */
+	public boolean isTargetable(int crosshairsX, int crosshairsY) {
+		return (crosshairsX > mX && crosshairsX < (mX + mWidth) && crosshairsY > mY && crosshairsY < (mY + mHeight));
+	}
+
+	/**
+	 * Hit the item with damage
+	 *
+	 * @param damage
+	 */
+	public void hit(int damage) {
+		mHealth = Math.max(0, mHealth - damage);
+	}
+
+	/**
+	 * Getters and Setters
+	 */
+	public int getHealth() {
+		return mHealth;
+	}
+
+	public void setHealth(int health) {
+		mHealth = health;
 	}
 }
