c0673deb62b5bcdc085eea6487bc21e3fcf03cd4
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 16:15:14 +0200

    Add Weapon Skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/Weapon.java
@@ -0,0 +1,86 @@
+package fr.tvbarthel.games.chasewhisply.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Weapon implements Parcelable {
+	private int mDamage;
+	private int mCurrentAmmunition;
+	private int mAmmunitionLimit;
+
+	public Weapon() {
+		mDamage = 0;
+		mCurrentAmmunition = 0;
+		mAmmunitionLimit = 0;
+	}
+
+	public Weapon(Parcel in) {
+		readFromParcel(in);
+	}
+
+	public void reload() {
+		if (mCurrentAmmunition < mAmmunitionLimit) {
+			mCurrentAmmunition += 1;
+		}
+	}
+
+	public int fire() {
+		if (mCurrentAmmunition > 0) {
+			mCurrentAmmunition -= 1;
+			return mDamage;
+		}
+		return 0;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int i) {
+		out.writeInt(mDamage);
+		out.writeInt(mCurrentAmmunition);
+		out.writeInt(mAmmunitionLimit);
+	}
+
+	public void readFromParcel(Parcel in) {
+		mDamage = in.readInt();
+		mCurrentAmmunition = in.readInt();
+		mAmmunitionLimit = in.readInt();
+	}
+
+	public static final Parcelable.Creator<Weapon> CREATOR = new Parcelable.Creator<Weapon>() {
+		public Weapon createFromParcel(Parcel in) {
+			return new Weapon(in);
+		}
+
+		public Weapon[] newArray(int size) {
+			return new Weapon[size];
+		}
+	};
+
+	/**
+	 * Getters & Setters
+	 */
+	public void setDamage(int damage) {
+		mDamage = damage;
+	}
+
+	public int getDamage() {
+		return mDamage;
+	}
+
+	public int getCurrentAmmunition() {
+		return mCurrentAmmunition;
+	}
+
+	public void setAmmunitionLimit(int ammunitionLimit) {
+		mAmmunitionLimit = ammunitionLimit;
+	}
+
+	public int getAmmunitionLimit() {
+		return mAmmunitionLimit;
+	}
+
+}
