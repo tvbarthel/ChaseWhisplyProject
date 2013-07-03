adeb9a64f4968f079ea5214cc2cfe89fe1989d09
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 17:08:21 +0200

    Comment a little

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/GameEngine.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/GameEngine.java
@@ -9,21 +9,33 @@ abstract public class GameEngine implements ReloadingRoutine.IReloadingRoutine {
 		mReloadingRoutine = new ReloadingRoutine(mGameInformation.getReloadingTime(), this);
 	}
 
+	/**
+	 * start the game, should be called only at the beginning once.
+	 */
 	public void startGame() {
 		mReloadingRoutine.startRoutine();
 		//TODO
 	}
 
+	/**
+	 * pause the game.
+	 */
 	public void pauseGame() {
 		mReloadingRoutine.stopRoutine();
 		//TODO
 	}
 
+	/**
+	 * resume the game
+	 */
 	public void resumeGame() {
 		mReloadingRoutine.startRoutine();
 		//TODO
 	}
 
+	/**
+	 * stop the game, should be called only once at the end.
+	 */
 	public void stopGame() {
 		mReloadingRoutine.stopRoutine();
 		//TODO
