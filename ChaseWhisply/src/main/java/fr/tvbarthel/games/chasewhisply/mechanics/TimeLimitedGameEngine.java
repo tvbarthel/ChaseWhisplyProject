48d7e0625526348716f23146d31ad73576daa302
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 17:01:11 +0200

    Add TimeLimiteGameEngine skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/TimeLimitedGameEngine.java
@@ -0,0 +1,8 @@
+package fr.tvbarthel.games.chasewhisply.mechanics;
+
+public class TimeLimitedGameEngine extends GameEngine{
+
+	public TimeLimitedGameEngine(GameInformation gameInformation) {
+		super(gameInformation);
+	}
+}
