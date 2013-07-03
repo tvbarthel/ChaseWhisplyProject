1b49bb3e8750da21e84b9c3379e2f226b0ba40b0
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-03 17:01:00 +0200

    Add google play services librairy

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
@@ -1,23 +1,36 @@
 package fr.tvbarthel.games.chasewhisply;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+
+import com.google.android.gms.games.GamesClient;
 
 public class HomeActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+	private GamesClient mGamesClient;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		findViewById(R.id.home_sign_in).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				//mGamesClient.connect();
+			}
+		});
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
