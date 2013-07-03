ac72912cd80f4e123513a87552ae89b8d3346fb7
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-03 21:28:14 +0200

    Basic Camera features implemented

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/GameActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/GameActivity.java
@@ -1,13 +1,34 @@
 package fr.tvbarthel.games.chasewhisply;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import fr.tvbarthel.games.chasewhisply.ui.CameraPreview;
+import fr.tvbarthel.games.chasewhisply.ui.GameView;
 
 public class GameActivity extends Activity {
+
+	private Camera mCamera;
+	private CameraPreview mCameraPreview;
+	private GameView mGameView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		mCamera = getCameraInstance();
+
+		if (mCamera == null) {
+			finish();
+		} else {
+			mCameraPreview = new CameraPreview(this, mCamera);
+			setContentView(mCameraPreview);
+			mGameView = new GameView(this);
+			addContentView(mGameView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT
+					, ViewGroup.LayoutParams.WRAP_CONTENT));
+		}
 	}
 
 	@Override
@@ -16,7 +37,31 @@ public class GameActivity extends Activity {
 	}
 
 	@Override
-	protected void onStart() {
-		super.onStart();
+	protected void onPause() {
+		super.onPause();
+		releaseCamera();
+	}
+
+	/**
+	 * A safe way to get an instance of the Camera object.
+	 */
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	/**
+	 * release camera properly
+	 */
+	private void releaseCamera() {
+		if (mCamera != null) {
+			mCamera.release();        // release the camera for other applications
+			mCamera = null;
+		}
 	}
 }
