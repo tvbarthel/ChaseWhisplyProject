9b523c31e3f11a9cf51fee9d6adde60cbe621625
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-03 15:53:31 +0200

    Rename TimeBasedRoutine in ReloadingRoutine

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/ReloadingRoutine.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/ReloadingRoutine.java
@@ -3,36 +3,25 @@ package fr.tvbarthel.games.chasewhisply.mechanics;
 
 import android.os.Handler;
 
-public class TimeBasedRoutine extends Handler {
+public class ReloadingRoutine extends Handler {
 	private final long mReloadingTime;
-	private final long mGameDuration;
-	private long mTimeCounter;
 	private final Runnable mRunnable;
 	private boolean mIsRunning;
 	private final ITimeBasedRoutine mInterface;
 
-	public TimeBasedRoutine(long reloadingTime, long gameDuration, ITimeBasedRoutine iTimeBasedRoutine) {
+	public ReloadingRoutine(long reloadingTime, ITimeBasedRoutine iTimeBasedRoutine) {
 		mReloadingTime = reloadingTime;
-		mGameDuration = gameDuration;
-		mTimeCounter = 0;
 		mIsRunning = false;
 		mInterface = iTimeBasedRoutine;
 		mRunnable = new Runnable() {
 			@Override
 			public void run() {
-				mTimeCounter += mReloadingTime;
 				mInterface.reload();
-				if (mTimeCounter < mGameDuration && mIsRunning) {
+				if (mIsRunning) {
 					postDelayed(this, mReloadingTime);
-				} else if (mTimeCounter >= mGameDuration) {
-					stopRoutine();
 				}
 			}
 		};
-	}
-
-	public void setTimeCounter(long timeCounter) {
-		mTimeCounter = timeCounter;
 	}
 
 	public void startRoutine() {
@@ -45,13 +34,10 @@ public class TimeBasedRoutine extends Handler {
 	public void stopRoutine() {
 		mIsRunning = false;
 		removeCallbacks(mRunnable);
-		mInterface.stop();
 	}
 
 	public interface ITimeBasedRoutine {
 		abstract public void reload();
-
-		abstract public void stop();
 	}
 
 }
