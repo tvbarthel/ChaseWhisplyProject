03c8ce7924a28dc68ae1c1339cf6014564d88c46
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-03 21:09:39 +0200

    Add GameView skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/GameView.java
@@ -0,0 +1,17 @@
+package fr.tvbarthel.games.chasewhisply.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameView extends View {
+
+	public GameView(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+}
