4ce60c898ffa7e4bf60455d793175a86bbec5786
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-07 16:46:12 +0200

    Add createEasyGhost to DisplayableItemFactory

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/DisplayableItemFactory.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/DisplayableItemFactory.java
@@ -1,4 +1,16 @@
 package fr.tvbarthel.games.chasewhisply.model;
 
 public class DisplayableItemFactory {
+	// Type
+	public final static int TYPE_EASY_GHOST = 0x00000001;
+
+	//Health
+	public final static int HEALTH_EASY_GHOST = 1;
+
+	public static TargetableItem createEasyGhost() {
+		TargetableItem easyGhost = new TargetableItem();
+		easyGhost.setType(TYPE_EASY_GHOST);
+		easyGhost.setHealth(HEALTH_EASY_GHOST);
+		return easyGhost;
+	}
 }
