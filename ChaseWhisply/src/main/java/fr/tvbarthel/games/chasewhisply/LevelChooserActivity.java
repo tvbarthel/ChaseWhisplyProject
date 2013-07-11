9936eeb981dff76538de5235ce44ef17108ba70f
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-11 15:09:01 +0200

    Add level chooser activity skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/LevelChooserActivity.java
@@ -0,0 +1,12 @@
+package fr.tvbarthel.games.chasewhisply;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LevelChooserActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
