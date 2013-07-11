5c9895b81e7e6cc27512f1552c5a23ed3dec5d97
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-11 16:26:44 +0200

    Add ScoreActivity

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ScoreActivity.java
@@ -0,0 +1,13 @@
+package fr.tvbarthel.games.chasewhisply;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_score);
+	}
+}
