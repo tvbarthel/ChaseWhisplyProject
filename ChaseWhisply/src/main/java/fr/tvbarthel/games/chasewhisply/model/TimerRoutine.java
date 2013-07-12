00ca03e4f6c01fd68892187149e13641c6159131
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-12 14:08:03 +0200

    Add TimerRoutine

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/TimerRoutine.java
@@ -0,0 +1,48 @@
+package fr.tvbarthel.games.chasewhisply.model;
+
+import android.os.Handler;
+
+abstract public class TimerRoutine extends Handler {
+	protected Runnable mRunnable;
+	protected boolean mIsRunning;
+	protected long mTickingTime;
+
+	/**
+	 * Create a TimerRoutine
+	 *
+	 * @param tickingTime ticking time in millisecond
+	 */
+	protected TimerRoutine(long tickingTime) {
+		mTickingTime = tickingTime;
+		mIsRunning = false;
+		mRunnable = new Runnable() {
+			@Override
+			public void run() {
+				doOnTick();
+				if (mIsRunning) {
+					postDelayed(this, mTickingTime);
+				}
+			}
+		};
+	}
+
+	/**
+	 * Start the TimerRoutine
+	 */
+	public void startRoutine() {
+		if (!mIsRunning) {
+			mIsRunning = true;
+			postDelayed(mRunnable, mTickingTime);
+		}
+	}
+
+	/**
+	 * Stop the TimerRoutine
+	 */
+	public void stopRoutine() {
+		mIsRunning = false;
+		removeCallbacks(mRunnable);
+	}
+
+	abstract protected void doOnTick();
+}
