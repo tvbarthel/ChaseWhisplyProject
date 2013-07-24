c19b92dfe19ff176f5f66c50d2347de251ab35a4
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-07-24 16:50:50 +0200

    Make achievement button works and use resultCode

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/HomeActivity.java
@@ -7,11 +7,12 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.games.GamesClient;
-
 import fr.tvbarthel.games.chasewhisply.google.BaseGameActivity;
 
 public class HomeActivity extends BaseGameActivity {
+
+	private static final int REQUEST_ACHIEVEMENT = 0x00000000;
+	private static final int REQUEST_LEADERBOARD = 0x00000001;
 
 	private Button mButtonSignIn;
 	private Button mLeaderBoard;
@@ -21,6 +22,7 @@ public class HomeActivity extends BaseGameActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
+		//sign in
 		mButtonSignIn = (Button) findViewById(R.id.home_sign_in);
 		mButtonSignIn.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -29,13 +31,25 @@ public class HomeActivity extends BaseGameActivity {
 			}
 		});
 
+		//leaderboard
 		mLeaderBoard = (Button) findViewById(R.id.home_leaderboard);
 		mLeaderBoard.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				if (getGamesClient().isConnected()) {
-					startActivityForResult(getGamesClient().getLeaderboardIntent(getResources().getString(R.string.leaderboard_easy)), 0);
+					startActivityForResult(getGamesClient().getLeaderboardIntent(
+							getResources().getString(R.string.leaderboard_easy)), REQUEST_LEADERBOARD);
 				} else {
+				}
+			}
+		});
+
+		//achievement
+		findViewById(R.id.home_achievement).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (getGamesClient().isConnected()) {
+					startActivityForResult(getGamesClient().getAchievementsIntent(), REQUEST_ACHIEVEMENT);
 				}
 			}
 		});
