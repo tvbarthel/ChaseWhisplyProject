f7cbc33f2d6ee683641410e55a4fc5a75db04c57
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-07-25 16:41:35 +0200

    Add about fragment skeleton

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/AboutFragment.java
@@ -0,0 +1,17 @@
+package fr.tvbarthel.games.chasewhisply.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fr.tvbarthel.games.chasewhisply.R;
+
+public class AboutFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_about, container, false);
+		return v;
+	}
+}
