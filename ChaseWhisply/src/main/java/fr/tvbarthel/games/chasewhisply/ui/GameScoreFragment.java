823c15348d7279213537a73576ece19e36b2ff61
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-07-31 10:33:29 +0200

    Implement View.OnClickListener

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/GameScoreFragment.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/GameScoreFragment.java
@@ -2,9 +2,16 @@ package fr.tvbarthel.games.chasewhisply.ui;
 
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class GameScoreFragment extends Fragment {
+import fr.tvbarthel.games.chasewhisply.R;
+
+public class GameScoreFragment extends Fragment implements View.OnClickListener {
+
 
     //interface
     public interface Listener {
@@ -29,4 +36,36 @@ public class GameScoreFragment extends Fragment {
                     + " must implemenet GameScoreFragment.Listener");
         }
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        final int[] clickable = new int[]{
+                R.id.score_button_replay,
+                R.id.score_button_home,
+                R.id.score_button_skip
+        };
+        for (int i : clickable) {
+            v.findViewById(i).setOnClickListener(this);
+        }
+        return v;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.score_button_home:
+                mListener.onHomeRequested();
+                break;
+            case R.id.score_button_skip:
+                mListener.onSkipRequested();
+                break;
+            case R.id.score_button_replay:
+                mListener.onReplayRequested();
+                break;
+        }
+
+    }
+
 }
