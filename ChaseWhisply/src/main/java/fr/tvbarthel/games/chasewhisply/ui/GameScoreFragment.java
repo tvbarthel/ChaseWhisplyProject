0f366ca24d28f29422b567aad2129d7b212b5ac3
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-07-31 10:17:37 +0200

    Add interface

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/GameScoreFragment.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/GameScoreFragment.java
@@ -2,4 +2,17 @@ package fr.tvbarthel.games.chasewhisply.ui;
 
 
 public class GameScoreFragment {
+
+    //interface
+    public interface Listener {
+        public void onReplayRequested();
+
+        public void onSkipRequested();
+
+        public void onHomeRequested();
+
+        public void onPushScoreRequested(int score);
+    }
+
+    private Listener mListener = null;
 }
