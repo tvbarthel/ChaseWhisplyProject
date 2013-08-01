79d98d0e4f20dd25dac9b6ce21a7860a9e56b84b
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-08-01 19:14:00 +0200

    (fix) Create world accordingly to the chosen game mode

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/GameInformationFactory.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/mechanics/GameInformationFactory.java
@@ -2,6 +2,8 @@ package fr.tvbarthel.games.chasewhisply.mechanics;
 
 
 import fr.tvbarthel.games.chasewhisply.model.DisplayableItemFactory;
+import fr.tvbarthel.games.chasewhisply.model.GameMode;
+import fr.tvbarthel.games.chasewhisply.model.GameModeFactory;
 import fr.tvbarthel.games.chasewhisply.model.WeaponFactory;
 
 public class GameInformationFactory {
@@ -45,6 +47,35 @@ public class GameInformationFactory {
 		demoWorld.setSceneHeight((int) Math.floor(cameraVerticalViewAngle));
 		demoWorld.setMaxTargetOnTheField(10);
 		return demoWorld;
+	}
+
+	public static GameInformation createEmptyWorld(float cameraHorizontalViewAngle, float cameraVerticalViewAngle, GameMode gameMode) {
+		final int gameType = gameMode.getType();
+		final int gameLevel = gameMode.getLevel();
+
+		GameInformation world = new GameInformation(DEFAULT_REMAINING_TIME, DEFAULT_SPAWNING_TIME, WeaponFactory.createBasicWeapon());
+		world.setSceneWidth((int) Math.floor(cameraHorizontalViewAngle));
+		world.setSceneHeight((int) Math.floor(cameraVerticalViewAngle));
+		world.setMaxTargetOnTheField(10);
+
+		if (gameType == GameModeFactory.GAME_TYPE_REMAINING_TIME) {
+			switch (gameLevel) {
+				case 1:
+					world.setRemainingTime(30000);
+					break;
+
+				case 2:
+					world.setRemainingTime(60000);
+					break;
+
+				case 3:
+					world.setRemainingTime(90000);
+					break;
+
+			}
+		}
+
+		return world;
 	}
 
 
