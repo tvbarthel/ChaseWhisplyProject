f3ce1eed70d7b36cf474cd15eb5b6fd02f21ff5c
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-08-02 20:15:43 +0200

    Reduce  reloading time

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/WeaponFactory.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/WeaponFactory.java
@@ -3,7 +3,7 @@ package fr.tvbarthel.games.chasewhisply.model;
 public class WeaponFactory {
 	private static final int BASIC_WEAPON_DAMAGE = 1;
 	private static final int BASIC_WEAPON_AMMUNITION_LIMIT = 8;
-	private static final long BASIC_WEAPON_RELOADING_TIME = 2000;
+	private static final long BASIC_WEAPON_RELOADING_TIME = 1500;
 
 	public static Weapon createBasicWeapon() {
 		Weapon basicWeapon = new Weapon();
