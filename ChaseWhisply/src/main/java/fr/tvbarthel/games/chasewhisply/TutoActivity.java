24fa1be13a58146b105fbee9ebd4248bc7a93580
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-08-05 17:56:10 +0200

    A fifth page to the tutorial

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/TutoActivity.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/TutoActivity.java
@@ -23,7 +23,7 @@ import fr.tvbarthel.games.chasewhisply.ui.TutoFragment;
 
 public class TutoActivity extends FragmentActivity implements ViewSwitcher.ViewFactory {
 	public static final String EXTRA_HELP_REQUESTED = "ExtraHelpRequested";
-	public static final int NB_PAGES = 4;
+	public static final int NB_PAGES = 5;
 	private static final String FIRST_LAUNCH_KEY = "First_launch_KEY";
 	private SharedPreferences mPrefs;
 	private String[] mPageTitles;
@@ -59,7 +59,8 @@ public class TutoActivity extends FragmentActivity implements ViewSwitcher.ViewF
 		mPageTitles = new String[]{getResources().getString(R.string.tuto_title_page_1),
 				getResources().getString(R.string.tuto_title_page_2),
 				getResources().getString(R.string.tuto_title_page_3),
-				getResources().getString(R.string.tuto_title_page_4)};
+				getResources().getString(R.string.tuto_title_page_4),
+				getResources().getString(R.string.tuto_title_page_5)};
 
 		//initialize title text switcher
 		mTitleSwitcher = (TextSwitcher) findViewById(R.id.tuto_text_switcher);
@@ -146,6 +147,9 @@ public class TutoActivity extends FragmentActivity implements ViewSwitcher.ViewF
 					break;
 				case 3:
 					layoutResId = R.layout.fragment_tuto_page_4;
+					break;
+				case 4:
+					layoutResId = R.layout.fragment_tuto_page_5;
 					break;
 				default:
 					layoutResId = R.layout.fragment_tuto_default_page;
