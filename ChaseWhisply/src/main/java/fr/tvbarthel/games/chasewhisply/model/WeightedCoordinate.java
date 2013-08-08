bf42baa8dd39c3458f84c793b9db26e5dabfe10e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-08-08 15:07:31 +0200

    Add a WeightedCoordinate class for future use

---
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/WeightedCoordinate.java
@@ -0,0 +1,73 @@
+package fr.tvbarthel.games.chasewhisply.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class WeightedCoordinate implements Parcelable {
+	private float mValue;
+	private float mWeight;
+
+	public WeightedCoordinate(float value, float weight) {
+		mValue = value;
+		mWeight = weight;
+	}
+
+	public WeightedCoordinate() {
+		mValue = 0;
+		mWeight = 0;
+	}
+
+	public WeightedCoordinate(Parcel in) {
+		readFromParcel(in);
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeFloat(mValue);
+		dest.writeFloat(mWeight);
+	}
+
+	public void readFromParcel(Parcel in) {
+		mValue = in.readFloat();
+		mWeight = in.readFloat();
+	}
+
+	public static final Parcelable.Creator<WeightedCoordinate> CREATOR = new Parcelable.Creator<WeightedCoordinate>() {
+		public WeightedCoordinate createFromParcel(Parcel in) {
+			return new WeightedCoordinate(in);
+		}
+
+		public WeightedCoordinate[] newArray(int size) {
+			return new WeightedCoordinate[size];
+		}
+	};
+
+	/**
+	 * Setters and Getters
+	 */
+
+	public void setWeight(float weight) {
+		mWeight = weight;
+	}
+
+	public float getWeight() {
+		return mWeight;
+	}
+
+	public void setValue(float value) {
+		mValue = value;
+	}
+
+	public float getValue() {
+		return mValue;
+	}
+
+	public float getWeightedValue() {
+		return mValue * mWeight;
+	}
+}
