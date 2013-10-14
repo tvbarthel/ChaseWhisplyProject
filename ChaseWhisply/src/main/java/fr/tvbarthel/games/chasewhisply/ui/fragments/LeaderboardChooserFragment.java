c0bc915a8913d0ef9b84d7229f08b1769d178951
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-10-14 16:38:42 +0200

    Hide "death to the king" Leaderboard for the public release

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/fragments/LeaderboardChooserFragment.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/fragments/LeaderboardChooserFragment.java
@@ -75,7 +75,7 @@ public class LeaderboardChooserFragment extends Fragment implements GameModeView
 		mGameModeViewAdapter.add(GameModeFactory.createSurvivalGame(1));
 
 		//Fifth mode: Death to the king
-		mGameModeViewAdapter.add(GameModeFactory.createKillTheKingGame(1));
+		//mGameModeViewAdapter.add(GameModeFactory.createKillTheKingGame(1));
 
 		mGameModeViewAdapter.notifyDataSetChanged();
 	}
