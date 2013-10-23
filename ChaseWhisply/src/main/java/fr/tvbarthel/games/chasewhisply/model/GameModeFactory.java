b81bae6ce136bd18daa2c894dabecd6b359ea3a7
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-10-23 21:42:36 +0200

    Fix required level

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/GameModeFactory.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/model/GameModeFactory.java
@@ -12,7 +12,7 @@ public class GameModeFactory {
 	public static final int GAME_TYPE_DEATH_TO_THE_KING = 0x00000004;
 	public static final int GAME_TYPE_OVERALL_RANKING = 0x00000005;
 	public static final int GAME_REQUIRED_LEVEL_SURVIVAL_MODE = 5;
-	public static final int GAME_REQUIRED_LEVEL_DEATH_TO_THE_KING = 1;
+	public static final int GAME_REQUIRED_LEVEL_DEATH_TO_THE_KING = 10;
 
 	/**
 	 * game mode for learn how to play.
