e347ebbace485f2ae4477505f7539286cd13001e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-04-01 22:05:00 +0200

    Use a view holder in BestiaryEntryAdapter

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/adapter/BestiaryEntryAdapter.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/adapter/BestiaryEntryAdapter.java
@@ -12,32 +12,46 @@ import fr.tvbarthel.games.chasewhisply.R;
 import fr.tvbarthel.games.chasewhisply.model.bestiary.BestiaryEntry;
 
 public class BestiaryEntryAdapter extends ArrayAdapter<BestiaryEntry> {
-	private BestiaryEntry[] mBestiaryEntries;
+    private BestiaryEntry[] mBestiaryEntries;
 
-	public BestiaryEntryAdapter(Context context, BestiaryEntry[] bestiaryEntries) {
-		super(context, R.layout.row_bestiary_entry, bestiaryEntries);
-		mBestiaryEntries = bestiaryEntries;
-	}
+    public BestiaryEntryAdapter(Context context, BestiaryEntry[] bestiaryEntries) {
+        super(context, R.layout.row_bestiary_entry, bestiaryEntries);
+        mBestiaryEntries = bestiaryEntries;
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		final Context context = getContext();
-		final BestiaryEntry currentBestiaryEntry = mBestiaryEntries[position];
-		final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View rowView = convertView;
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Context context = getContext();
+        final BestiaryEntry currentBestiaryEntry = mBestiaryEntries[position];
+        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = convertView;
 
-		if (rowView == null) {
-			rowView = inflater.inflate(R.layout.row_bestiary_entry, parent, false);
-		}
+        if (rowView == null) {
+            rowView = inflater.inflate(R.layout.row_bestiary_entry, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.entryTitle = (TextView) rowView.findViewById(R.id.row_bestiary_entry_title);
+            viewHolder.entryImage = (ImageView) rowView.findViewById(R.id.row_bestiary_entry_image);
+            viewHolder.health = (TextView) rowView.findViewById(R.id.bestiary_health);
+            viewHolder.point = (TextView) rowView.findViewById(R.id.bestiary_point);
+            viewHolder.exp = (TextView) rowView.findViewById(R.id.bestiary_exp);
+            rowView.setTag(viewHolder);
+        }
 
-		((TextView) rowView.findViewById(R.id.row_bestiary_entry_title)).setText(currentBestiaryEntry.getTitleResourceId());
-		((ImageView) rowView.findViewById(R.id.row_bestiary_entry_image)).setImageResource(currentBestiaryEntry.getImageResourceId());
-		((TextView) rowView.findViewById(R.id.bestiary_health)).setText(currentBestiaryEntry.getHealth());
-		((TextView) rowView.findViewById(R.id.bestiary_point)).setText(currentBestiaryEntry.getPointValue());
-		((TextView) rowView.findViewById(R.id.bestiary_exp)).setText(currentBestiaryEntry.getExpValue());
-		
+        final ViewHolder viewHolder = (ViewHolder) rowView.getTag();
+        viewHolder.entryTitle.setText(currentBestiaryEntry.getTitleResourceId());
+        viewHolder.entryImage.setImageResource(currentBestiaryEntry.getImageResourceId());
+        viewHolder.health.setText(currentBestiaryEntry.getHealth());
+        viewHolder.point.setText(currentBestiaryEntry.getPointValue());
+        viewHolder.exp.setText(currentBestiaryEntry.getExpValue());
 
-		return rowView;
+        return rowView;
+    }
 
-	}
+    private final class ViewHolder {
+        public TextView entryTitle;
+        public ImageView entryImage;
+        public TextView health;
+        public TextView point;
+        public TextView exp;
+    }
 }
