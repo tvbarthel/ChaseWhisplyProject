18a4349aabd0c23ffc49d46f5b9c677b2f4eafa4
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2014-05-29 19:34:11 +0200

    Redesign in game user interface

--- ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/gameviews/GameViewTime.java
+++ ChaseWhisply/src/main/java/fr/tvbarthel/games/chasewhisply/ui/gameviews/GameViewTime.java
@@ -3,6 +3,7 @@ package fr.tvbarthel.games.chasewhisply.ui.gameviews;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.RectF;
 
 import fr.tvbarthel.games.chasewhisply.mechanics.engine.GameEngineTime;
 
@@ -28,13 +29,23 @@ public class GameViewTime extends GameViewStandard {
     protected void drawTimer(Canvas canvas) {
         final long millis = mGameEngine.getCurrentTime();
         final int seconds = (int) (millis / 1000);
-        final String remainingTime = String.format(mTimeString, seconds);
+        final String remainingTime = String.valueOf(seconds);
+        final int radius = Math.max(mTimerBitmap.getWidth(), mTimerBitmap.getHeight()) + (int) mPadding;
         resetPainter();
-        useGreenPainter();
+
+        //draw transparent overlay
+        useTransparentBlackPainter();
+        canvas.drawOval(new RectF(-radius, -radius, radius, radius), mPaint);
+
+        //draw icon
+        useWhitePainter();
+        canvas.drawBitmap(mTimerBitmap, 0, 0, mPaint);
+
+        //draw time
         mPaint.getTextBounds(remainingTime, 0, remainingTime.length(), mBounds);
         canvas.drawText(remainingTime
-                , mPadding + mBounds.width() / 2
-                , mPadding + mPaint.getTextSize()
+                , mPadding + radius
+                , radius
                 , mPaint);
 
     }
